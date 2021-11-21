44a0ae3461b26c6e013f45b58a5e61e2949889b9
Lev Kaplunov <limon4ik59rus@mail.ru>
2021-11-21 14:38:33 +0500

    - Добалены в игнор лишние файлы - Создан пример файла конфига

---
+++ example.config.js
@@ -0,0 +1,19 @@
+export const TOKEN = 'token';
+
+/**
+ * Время действия опроса в секундах
+ * @type {number}
+ */
+export const POLL_TIMEOUT = 1800;
+
+/**
+ * Database credentials
+ */
+export const DB_NAME = 'name';
+export const DB_USER = 'user';
+export const DB_PASSWORD = 'password';
+export const DB_SERVER = 'server';
+export const DB_PORT = 'port';
+export const DB_SOKET_PATH = 'socket';
+
+export const BOT_NAME = 'botname';
