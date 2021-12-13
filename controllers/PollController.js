10a49e925eefc29e83cb9097079a217a4879c1ae
Lev Kaplunov <limon4ik59rus@mail.ru>
2021-12-13 10:38:57 +0500

    Убрал не актуальный функционал

--- controllers/PollController.js
+++ controllers/PollController.js
@@ -1,20 +1,33 @@
 import { appBot } from '../index';
-import { ADDITIONAL_POLL_TEXT, MAIN_POLL_TEXT } from '../lang';
-import { additionalPoll, mainPoll } from '../poll';
+import { MAIN_POLL_TEXT } from '../lang';
 import { Group } from '../models/Group';
+import { Product } from '../models/Product';
+import db from 'sequelize';
 
+/**
+ * Создаёт опрос с выборкой случаныйх пицц
+ *
+ * @param {object} msg
+ * @return {Promise<void>}
+ */
 async function createPoll(msg) {
   const chatId = msg.chat.id;
-  await appBot.bot.sendPoll(chatId, MAIN_POLL_TEXT, mainPoll, {
+
+  const random = await Product.findAll({
+    order: db.Sequelize.literal('rand()'),
+    limit: 10
+  });
+
+  await appBot.bot.sendPoll(chatId, MAIN_POLL_TEXT, random.map(product => product.name), {
     is_anonymous: false,
     allows_multiple_answers: true
   });
-
-  await appBot.bot.sendPoll(chatId, ADDITIONAL_POLL_TEXT, additionalPoll, {
-    is_anonymous: false
-  });
 }
 
+/**
+ * Создаёт опросы всех групп, добавленных в бд
+ * @return {Promise<void>}
+ */
 async function createAllPolls() {
   const groups = await Group.findAll();
 
